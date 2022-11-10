8c6c6f648b985794d86d7b742f903ad15ad3da9b
kaine <snsh.shinra@gmail.com>
2022-11-10 23:17:21 +0300

    Some changes

--- events/bot_set_activity.py
+++ events/bot_set_activity.py
@@ -1,5 +1,4 @@
 import random
-
 from settings.config import settings
 import discord
 from discord.ext import commands, tasks
@@ -10,15 +9,17 @@ bot = commands.Bot(command_prefix=settings['prefix'], case_insensitive=True, int
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="/help"))
+    await change_presence.start()
 
 
-@tasks.loop(hours=1)
+@tasks.loop(minutes=30)
 async def change_presence():
     rand_num = random.randint(1, 3)
     if rand_num == 1:
         await bot.change_presence(activity=discord.Game(name="/help"))
     if rand_num == 2:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"за {len(bot.guilds)} серверами."))
+        await bot.change_presence(
+            activity=discord.Activity(type=discord.ActivityType.watching, name=f"за {len(bot.guilds)} серверами."))
     if rand_num == 3:
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"за котиками."))
 
