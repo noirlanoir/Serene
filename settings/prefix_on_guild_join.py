fdf9f2fb61afed34b6e245a7520b46e735e76dd7
kaine <snsh.shinra@gmail.com>
2022-11-10 22:49:39 +0300

    Removed several mistakes.

--- settings/prefix_on_guild_join.py
+++ settings/prefix_on_guild_join.py
@@ -6,16 +6,16 @@ from settings.config import settings
 bot = commands.Bot(command_prefix=settings['prefix'], intents=discord.Intents.all())
 
 curr_dir = (os.path.abspath(os.curdir))
-project_dir = os.path.dirname(abs_path)
+project_dir = os.path.dirname(curr_dir)
 prefix_dir = project_dir + '\settings\prefix.json'
 
 
 @bot.event
 async def on_guild_join(guild):
-    with open('C:/Users/Andrew/PycharmProjects/seren/settings/prefix.json', 'r') as f:
+    with open(prefix_dir, 'r') as f:
         prefix = json.load(f)
     prefix[str(guild.id)] = '-'
-    with open('C:/Users/Andrew/PycharmProjects/seren/settings/prefix.json', 'w') as w:
+    with open(prefix_dir, 'w') as w:
         json.dump(prefix, w, indent=4)
 
 
