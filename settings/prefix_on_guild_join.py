e7ef6ee306a70382dffc778bca515026c39661eb
kaine <snsh.shinra@gmail.com>
2022-11-13 22:05:25 +0300

    Removed mistake, added slash command not found error ignore

--- settings/prefix_on_guild_join.py
+++ settings/prefix_on_guild_join.py
@@ -2,12 +2,22 @@ import json
 import discord
 from discord.ext import commands
 from settings.config import settings
+import os
 
 bot = commands.Bot(command_prefix=settings['prefix'], intents=discord.Intents.all())
+bot.remove_command('help')
 
 curr_dir = (os.path.abspath(os.curdir))
 project_dir = os.path.dirname(curr_dir)
 prefix_dir = project_dir + '\settings\prefix.json'
+
+
+@bot.tree.error
+async def on_app_command_error(interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
+    if interaction:
+        return
+    if error:
+        return
 
 
 @bot.event
