f736f214728dbcf9dedb4b909f56d4731b084a1d
kaine <snsh.shinra@gmail.com>
2022-11-13 22:04:39 +0300

    ⠀

--- slash_commands/help_slash.py
+++ slash_commands/help_slash.py
@@ -5,25 +5,8 @@ from discord.ext import commands
 import os
 import json
 
-bot = commands.Bot(command_prefix=settings['prefix'], case_insensitive=True, intents=discord.Intents.all())
 
-
-@bot.event
-async def on_ready():
-    synced = await bot.tree.sync()
-    print(len(synced))
-
-
-@bot.tree.error
-async def on_app_command_error(interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
-    if interaction:
-        return
-    if error:
-        return
-
-
-@bot.tree.command(name='хелп', description='Показывает всю информацию о боте и командах.')
-async def help(interaction: discord.Interaction, команда: str = None):
+async def _help(interaction: discord.Interaction, команда: str = None):
     global c_d
     command = команда
     if command is None:
@@ -72,8 +55,7 @@ async def help(interaction: discord.Interaction, команда: str = None):
             value='`info`. ', inline=False
         )
         embed.add_field(name='⠀', value='⠀', inline=False)
-        embed.add_field(name='Доп. возможности:', value=
-                        '`Для того, чтобы узнать описание команды, впишите ее после /хелп.\nВсе команды с пометкой "[/]" являются слэш командами.\nВсе команды с пометкой "[Prefix]" являются командами,\n которые работают по префиксу бота.`')
+        embed.add_field(name='Доп. возможности:', value='`Для того, чтобы узнать описание команды, впишите ее после /хелп.\nВсе команды с пометкой "[/]" являются слэш командами.\nВсе команды с пометкой "[Prefix]" являются командами,\n которые работают по префиксу бота.`')
         await interaction.response.send_message(embed=embed)
     if command:
         curr_dir = (os.path.abspath(os.curdir))
@@ -89,6 +71,3 @@ async def help(interaction: discord.Interaction, команда: str = None):
             await interaction.response.send_message(
                 f'Команда `{c_d}` не найдена. Проверьте правильность написания команды либо просмотрите весь список через `/help`.',
                 ephemeral=True)
-
-
-bot.run(settings['token'])
