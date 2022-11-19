608f071a743b7cfedf819d03ea688b956da53f31
kaine <snsh.shinra@gmail.com>
2022-11-20 00:27:06 +0300

    ⠀

---
+++ slash_commands/nekofinder.py
@@ -0,0 +1,15 @@
+import random
+import requests
+from bs4 import BeautifulSoup
+import re
+from settings.config import settings
+import discord
+from discord.ext import commands
+import hmtai
+from discord import app_commands
+
+
+async def _nekoFinder(interaction: discord.Interaction):
+    embed = discord.Embed(title='Вот, что я нашла:', color=0x9900ff)
+    embed.set_image(url=hmtai.get('nekos', 'neko'))
+    await interaction.response.send_message(embed=embed)
